38fc63dff1619aed1e4f6203c7a1f5409b18bd4e
Atsushi Hanamoto <hanamoto@leapmind.io>
2020-04-18 21:59:39 +0900

    fix

--- src/dataset_test.py
+++ src/dataset_test.py
@@ -5,7 +5,7 @@ import dataset
 
 class TestMVTecDataset(unittest.TestCase):
 
-    mvtec_dataset = dataset.MVTecDataset("../sample_data/", ".png", "train", neg_dir="train/good")
+    mvtec_dataset = dataset.MVTecDataset("./sample_data/", ".png", "train", neg_dir="train/good")
 
     def test_dataset(self):
         self.assertEqual(len(self.mvtec_dataset), 10)
