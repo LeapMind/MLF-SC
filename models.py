f81b87ef16c8f56a92ec46f6313924cfd42bc406
Atsushi Hanamoto <hanamoto@leapmind.io>
2020-04-18 18:05:57 +0900

    merge test and visualize

--- models.py
+++ models.py
@@ -80,7 +80,7 @@ class SparseCodingWithMultiDict(object):
         with open(file_path, "rb") as f:
             self.dictionaries = pickle.load(f)
 
-    def test(self):
+    def test(self, org_H, org_W, patch_size, stride):
         C = len(self.dictionaries)
         coders = [
             SparseCoder(
@@ -92,16 +92,18 @@ class SparseCodingWithMultiDict(object):
         ]
 
         neg_err = self.calculate_error(
-            coders=coders, mode="neg", desc="testing for negative sample"
+            coders=coders, mode="neg", desc="testing for negative sample",
+            org_H=org_H, org_W=org_W, patch_size=patch_size, stride=stride
         )
         pos_err = self.calculate_error(
-            coders=coders, mode="pos", desc="testing for positive sample"
+            coders=coders, mode="pos", desc="testing for positive sample",
+            org_H=org_H, org_W=org_W, patch_size=patch_size, stride=stride
         )
 
         ap, auc = self.calculate_score(neg_err, pos_err)
         print("\nTest set: AP: {:.4f}, AUC: {:.4f}\n".format(ap, auc))
 
-    def calculate_error(self, coders, mode, desc=None):
+    def calculate_error(self, coders, mode, desc, org_H, org_W, patch_size, stride):
         if mode == "neg":
             loader = self.test_neg_loader
         elif mode == "pos":
@@ -112,19 +114,29 @@ class SparseCodingWithMultiDict(object):
         errs = []
         top_5 = numpy.zeros(len(self.dictionaries))
         for batch_data in tqdm(loader, desc=desc):
-            batch_img = batch_data[1]
+            batch_name, batch_img = batch_data[0], batch_data[1]
+            p_batch_img = batch_img
             for p in self.preprocesses:
-                batch_img = p(batch_img)
+                p_batch_img = p(p_batch_img)
 
-            for img in batch_img:
+            for img, img_org in zip(p_batch_img, batch_img):
                 P, C, H, W = img.shape
                 img_arr = img.reshape(P, C, H * W)
+                f_diff = numpy.zeros((1, org_H, org_W))
 
                 ch_err = []
                 for i in range(C):
                     target_arr = img_arr[:, i]
                     coefs = coders[i].transform(target_arr)
                     rcn_arr = coefs.dot(self.dictionaries[i])
+
+                    f_img_org = self.reconst_from_array(
+                        target_arr, org_H, org_W, patch_size, stride
+                    )
+                    f_img_rcn = self.reconst_from_array(
+                        rcn_arr, org_H, org_W, patch_size, stride
+                    )
+                    f_diff += numpy.square((f_img_org - f_img_rcn) / 2)
 
                     if not self.use_ssim:
                         err = numpy.sum((target_arr - rcn_arr) ** 2, axis=1)
@@ -143,81 +155,10 @@ class SparseCodingWithMultiDict(object):
                         ]
                     sorted_err = numpy.sort(err)[::-1]
                     total_err = numpy.sum(sorted_err[:5])
-
                     ch_err.append(total_err)
+
                 top_5[numpy.argsort(ch_err)[::-1][:5]] += 1
-
                 errs.append(numpy.sum(ch_err))
-
-        return errs
-
-    def calculate_score(self, dn, dp):
-        N = len(dn)
-        y_score = numpy.concatenate([dn, dp])
-        y_true = numpy.zeros(len(y_score), dtype=numpy.int32)
-        y_true[N:] = 1
-        return average_precision_score(y_true, y_score), roc_auc_score(y_true, y_score)
-
-    def visualize(self, ch, org_H, org_W, patch_size, stride):
-        coder = SparseCoder(
-            dictionary=self.dictionaries[ch],
-            transform_algorithm=self.transform_algorithm,
-            transform_n_nonzero_coefs=self.num_of_nonzero,
-        )
-
-        self.visualize_features(
-            coder=coder,
-            mode="neg",
-            ch=ch,
-            org_H=org_H,
-            org_W=org_W,
-            patch_size=patch_size,
-            stride=stride,
-            desc="visualizing for negative sample",
-        )
-        self.visualize_features(
-            coder=coder,
-            mode="pos",
-            ch=ch,
-            org_H=org_H,
-            org_W=org_W,
-            patch_size=patch_size,
-            stride=stride,
-            desc="visualizing for positive sample",
-        )
-
-    def visualize_features(
-        self, coder, mode, ch, org_H, org_W, patch_size, stride, desc=None
-    ):
-        if mode == "neg":
-            loader = self.test_neg_loader
-        elif mode == "pos":
-            loader = self.test_pos_loader
-        else:
-            raise ValueError("The argument 'mode' must be set to 'neg' or 'pos'.")
-
-        for idx, batch_data in tqdm(enumerate(loader), desc=desc):
-            batch_name, batch_img = batch_data[0], batch_data[1]
-            p_batch_img = batch_img
-            for p in self.preprocesses:
-                p_batch_img = p(p_batch_img)
-
-            for img, img_org in zip(p_batch_img, batch_img):
-                P, C, H, W = img.shape
-                img_arr = img.reshape(P, C, H * W)
-                f_diff = numpy.zeros((1, org_H, org_W))
-
-                for i in range(C):
-                    target_arr = img_arr[:, i]
-                    coefs = coder.transform(target_arr)
-                    rcn_arr = coefs.dot(self.dictionaries[i])
-                    f_img_org = self.reconst_from_array(
-                        target_arr, org_H, org_W, patch_size, stride
-                    )
-                    f_img_rcn = self.reconst_from_array(
-                        rcn_arr, org_H, org_W, patch_size, stride
-                    )
-                    f_diff += numpy.square((f_img_org - f_img_rcn) / 2)
 
                 f_diff /= C
                 color_map = plt.get_cmap("viridis")
@@ -238,9 +179,6 @@ class SparseCodingWithMultiDict(object):
                 output_path = os.path.join("visualized_results", mode)
                 os.makedirs(output_path, exist_ok=True)
 
-                err = numpy.sum((target_arr - rcn_arr) ** 2, axis=1)
-                sorted_err = numpy.sort(err)[::-1]
-                total_err = numpy.sum(sorted_err[:5])
                 cv2.imwrite(
                     os.path.join(
                         output_path,
@@ -248,6 +186,15 @@ class SparseCodingWithMultiDict(object):
                     ),
                     blended_out,
                 )
+
+        return errs
+
+    def calculate_score(self, dn, dp):
+        N = len(dn)
+        y_score = numpy.concatenate([dn, dp])
+        y_true = numpy.zeros(len(y_score), dtype=numpy.int32)
+        y_true[N:] = 1
+        return average_precision_score(y_true, y_score), roc_auc_score(y_true, y_score)
 
     def reconst_from_array(self, arrs, org_H, org_W, patch_size, stride):
         rcn = numpy.zeros((1, org_H, org_W))
